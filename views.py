445db852be595883b1e8be4563c1f02da06083a5
htcvaibhav <vaibhav.kumar@hindustantimes.com>
2013-12-03 12:23:33 +0530

    modified Views.py

--- views.py
+++ views.py
@@ -13,12 +13,6 @@ def email_messages_listing(request):
 		file_list_objs = [os.path.join("%s" % settings.EMAIL_FILE_PATH,d) for d in os.listdir('%s' % settings.EMAIL_FILE_PATH) if not os.path.isdir(d)]
 		file_list_objs = sorted(file_list_objs, key=lambda x: os.path.getctime(x), reverse=True)[:settings.KEEP_FILES_COUNT]
 
-		for each_file_list in file_list_objs:
-			file_ext = each_file_list.split('.')
-			if each_file_list.endswith(".log"):
-				file_ext[1] = 'html'
-				file_ext = ".".join(file_ext)
-				os.rename('%s' % (each_file_list),'%s' % (file_ext))
 		paginator = Paginator(file_list_objs, settings.LIST_EMAIL_MESSAGES_PER_PAGE)
 		try:
 			file_list = paginator.page(page)
